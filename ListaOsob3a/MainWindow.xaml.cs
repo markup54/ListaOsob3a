46bf94492e714896393cf237b7bc8bb0d106cf92
markup54 <marzenakupka@tlen.pl>
2021-12-16 09:55:39 +0100

    Dodaj pliki projektów.

---
+++ ListaOsob3a/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ListaOsob3a
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Osoba> ListaOsob { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PrzygotujWiazanie();
+        }
+
+        private void PrzygotujWiazanie()
+        {
+            ListaOsob = new ObservableCollection<Osoba>();
+            ListaOsob.Add(new Osoba("Jaś", "Gruszka", 12, "Zabrze"));
+            ListaOsob.Add(new Osoba("Gosia", "Śliwka", 12, "Zabrze"));
+            ListaOsob.Add(new Osoba("Zosia", "Malina", 12, "Zabrze"));
+            ListaOsob.Add(new Osoba("Ela", "Czereśnia", 12, "Zabrze"));
+            DataContext = this;
+        }
+    }
+}
---
+++ ListaOsob3a/Osoba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaOsob3a
+{
+    public class Osoba : ICloneable
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int Wiek { get; set; }
+        public string Miasto { get; set; }
+
+        public Osoba(string imie, string nazwisko, int wiek, string miasto)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Wiek = wiek;
+            Miasto = miasto;
+        }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
